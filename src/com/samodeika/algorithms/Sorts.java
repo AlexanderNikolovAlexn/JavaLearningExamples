3ff029afffb99d6586da4da5dc0b555e819ab2b2
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-11-12 13:38:20 +0200

    refactoring sort methods into one class Sorts

--- src/com/samodeika/algorithms/Sorts.java
+++ src/com/samodeika/algorithms/Sorts.java
@@ -2,21 +2,28 @@ package com.samodeika.algorithms;
 
 import java.util.Arrays;
 
-public final class BubbleSort {
+public class Sorts {
 
     public static void main(String[] args) {
+
         int[] arr = TestData.getSmallArr();
+        int[] arr2 = Arrays.copyOf(arr, arr.length);
+        System.out.println("Original array!");
         System.out.println(Arrays.toString(arr));
 
-        sort(arr);
+        bubbleSort(arr);
+        System.out.println("Bubble sort!");
+        System.out.println(Arrays.toString(arr));
+
+        insertionSort(arr);
+        System.out.println("Bubble sort!");
         System.out.println(Arrays.toString(arr));
 
     }
 
-    public static void sort(int[] arr) {
+    public static void bubbleSort(int[] arr) {
         int length = arr.length;
         boolean exit = true;
-
         while (exit) {
             exit = false;
             for (int i = 0; i < length - 1; i++) {
@@ -26,13 +33,23 @@ public final class BubbleSort {
                 }
             }
         }
+    }
 
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            while ((j - 1 >= 0) && (arr[j - 1] > arr[j])) {
+                swap(arr, j - 1, j);
+                j--;
+            }
+        }
     }
 
     private static void swap(int[] arr, int left, int right) {
         int tmp = arr[left];
         arr[left] = arr[right];
         arr[right] = tmp;
-    };
+    }
 
+    ;
 }
