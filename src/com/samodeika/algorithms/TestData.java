6984a0c544dc4a47cdce67713fc8cac72b7e54c4
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-11-12 12:07:03 +0200

    small refactoring for TestData

--- src/com/samodeika/algorithms/TestData.java
+++ src/com/samodeika/algorithms/TestData.java
@@ -5,13 +5,27 @@ import java.util.Random;
 public final class TestData {
 
     private static final int SMALL = 10;
+    private static final int AVG = 1000;
+    private static final int BIG = 10000;
 
     public static int[] getSmallArr() {
-        int[] arr = new int[SMALL];
+        return getArr(SMALL);
+    }
+
+    public static int[] getAvgArr() {
+        return getArr(AVG);
+    }
+
+    public static int[] getBigArr() {
+        return getArr(BIG);
+    }
+
+    private static int[] getArr(int amount) {
+        int[] arr = new int[amount];
         Random ran = new Random();
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = ran.nextInt(SMALL);
+            arr[i] = ran.nextInt(amount);
         }
 
         return arr;
