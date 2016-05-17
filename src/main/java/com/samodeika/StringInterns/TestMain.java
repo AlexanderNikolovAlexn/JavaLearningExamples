ffe483f3502a3f9a60d3e25eecc70c1b39df8803
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2016-05-17 10:01:08 +0300

    String intern example

---
+++ src/main/java/com/samodeika/StringInterns/TestMain.java
@@ -0,0 +1,28 @@
+package com.samodeika.StringInterns;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        String s1 = "alex";
+        String s2 = "alex";
+        String s3 = "alex".intern();
+
+        if (s1 == s2) {
+            System.out.println("s1 are equal to s2");
+        }
+
+        if (s1 == s3) {
+            System.out.println("s1 are equal to s3");
+        }
+
+        //String s1Concat = "alex" + "nikolov";
+        String s1Concat = "alex";
+        s1Concat = s1Concat + "nikolov";
+        String s2Concat = "alexnikolov";
+
+        if (s1Concat == s2Concat) {
+            System.out.println("s1Concat and s2Concat are equals");
+        }
+    }
+
+}
