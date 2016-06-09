dec99350f9a80a03c9e4da57eda40555876ac798
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2016-06-09 14:29:36 +0300

    added test main methods

---
+++ src/main/java/com/samodeika/temp/TestCollection.java
@@ -0,0 +1,10 @@
+package com.samodeika.temp;
+
+/**
+ * Created by PC on 9.6.2016 ?..
+ */
+public class TestCollection {
+
+
+
+}
---
+++ src/main/java/com/samodeika/temp/TestMain.java
@@ -0,0 +1,12 @@
+package com.samodeika.temp;
+
+/**
+ * Created by PC on 9.6.2016 ?..
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println("Test output");
+    }
+
+}
